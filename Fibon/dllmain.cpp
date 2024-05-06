9bc557cfdfc5cece68cca10c2fe95b06e0dd1e72
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-05-06 17:54:00 +0300

    Добавьте файлы проекта.

---
+++ Fibon/dllmain.cpp
@@ -0,0 +1,35 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+
+#include <iostream>
+
+
+static unsigned long long previous;
+static unsigned long long current;
+static unsigned index;
+
+void fibonacci_init(const unsigned long long a, const unsigned long long b) {
+    previous = b;
+    current = a;
+    index = 1;
+}
+
+bool fibonacci_next() {
+    if (current > ULLONG_MAX - previous) {
+        return false; // переполнение
+    }
+
+    unsigned long long next = previous + current;
+    previous = current;
+    current = next;
+    index++;
+    return true;
+}
+
+unsigned long long fibonacci_current() {
+    return current;
+}
+
+unsigned fibonacci_index() {
+    return index;
+}
+
